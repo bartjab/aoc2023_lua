ee9f9e3dfb0601e986d8f4d9a880cc88d0114f8f
Bartosz Rosadzinski <voltermc@gmail.com>
2023-12-01 16:58:00 +0100

    Initial commit
    Implemented a template for input loading.
    Created Day 01 stub.

---
+++ src/day01.lua
@@ -0,0 +1,10 @@
+-- INPUT READING BEGINS HERE
+if test_input then
+    local input_file = io.open("day01_small_input.txt", r)
+    io.input(input_file)
+else
+    local input_file = io.open("day01_large_input.txt", r)
+    io.input(input_file)
+end
+
+-- PROBLEM IMPLEMENTATION BEGINS HERE
---
+++ src/main.lua
@@ -0,0 +1,5 @@
+-- determines whether you want to use the small input (dayX_input_small.txt)
+-- or the large, personalized input (dayX_input_large.txt) (REMEMBER TO PASTE YOUR OWN INPUT)
+small_input = true
+
+dofile("day01.lua")
